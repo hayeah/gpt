026653fd4d0af67406be7f5499386cd033879db6
Howard Yeh <hayeah@gmail.com>
2024-06-01 10:09:54 +0800

    remove unused table

--- migrations/01_init.up.down.sql
+++ migrations/01_init.up.down.sql
@@ -1,1 +1,1 @@
-DROP TABLE IF EXISTS assistants;
+DROP TABLE IF EXISTS keys;
--- migrations/01_init.up.sql
+++ migrations/01_init.up.sql
@@ -1,9 +1,3 @@
-CREATE TABLE IF NOT EXISTS assistants (
-    id INTEGER PRIMARY KEY,
-    data TEXT NOT NULL,
-    CONSTRAINT valid_json CHECK (json_valid(data))
-) STRICT;
-
 CREATE TABLE IF NOT EXISTS keys (
     key TEXT PRIMARY KEY,
     value TEXT NOT NULL,
