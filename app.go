f8d518bf8431659c878561355363d6d93db3f81b
Howard Yeh <hayeah@gmail.com>
2024-06-03 11:14:22 +0800

    assistant into its own file

--- app.go
+++ app.go
@@ -2,10 +2,6 @@ package gpt
 
 import (
 	"context"
-	"crypto/sha256"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
 
 	"github.com/hayeah/goo"
 	"github.com/hayeah/goo/fetch"
@@ -21,27 +17,6 @@ type App struct {
 	RunManager       *RunManager
 	ThreadRunner     *ThreadRunner
 	// Migrate          *migrate.Migrate
-}
-
-// hashAssistantRequest prepends sha256 to the description
-func hashAssistantRequest(aReq *openai.AssistantRequest) error {
-	aReqJSON, err := json.Marshal(aReq)
-	if err != nil {
-		return err
-	}
-
-	hashed := sha256.Sum256(aReqJSON)
-
-	metadata := aReq.Metadata
-
-	if metadata == nil {
-		metadata = make(map[string]interface{})
-		aReq.Metadata = metadata
-	}
-
-	metadata["__hash__"] = hex.EncodeToString(hashed[:])
-
-	return nil
 }
 
 func (a *App) Run() error {
@@ -173,88 +148,6 @@ func (rm *RunManager) ListSteps() error {
 	goo.PrintJSON(steps)
 
 	return nil
-}
-
-type AssistantManager struct {
-	OAI    *OpenAIClientV2
-	JSONDB *JSONDB
-}
-
-// Show retrieves assistant info
-func (am *AssistantManager) Show(assistantID string) error {
-	var err error
-	if assistantID == "" {
-		assistantID, err = am.CurrentAssistantID()
-		if err != nil {
-			return err
-		}
-	}
-
-	assistant, err := am.OAI.RetrieveAssistant(context.Background(), assistantID)
-	if err != nil {
-		return err
-	}
-
-	goo.PrintJSON(assistant)
-
-	return nil
-
-}
-
-func (am *AssistantManager) List() error {
-	as, err := am.OAI.ListAssistants(context.Background(), nil, nil, nil, nil)
-	if err != nil {
-		return err
-	}
-
-	goo.PrintJSON(as)
-
-	return nil
-}
-
-func (am *AssistantManager) Create(filePath string) error {
-	var aReq openai.AssistantRequest
-	err := goo.DecodeFile(filePath, &aReq)
-	if err != nil {
-		return err
-	}
-
-	err = hashAssistantRequest(&aReq)
-	if err != nil {
-		return err
-	}
-
-	oai := am.OAI
-
-	ctx := context.Background()
-
-	assistant, err := oai.CreateAssistant(ctx, aReq)
-	if err != nil {
-		return err
-	}
-
-	goo.PrintJSON(assistant)
-
-	return am.JSONDB.Put("currentAssistant", assistant.ID)
-	// return nil
-}
-
-// Use selects an assistant
-func (am *AssistantManager) Use(assistantID string) error {
-	return am.JSONDB.Put("currentAssistant", assistantID)
-}
-
-// CurrentAssistantID
-func (am *AssistantManager) CurrentAssistantID() (string, error) {
-	var assistantID string
-	ok, err := am.JSONDB.Get("currentAssistant", &assistantID)
-	if err != nil {
-		return "", err
-	}
-	if !ok {
-		return "", fmt.Errorf("no current assistant")
-	}
-	return assistantID, nil
 }
 
 type OAIClient struct {
